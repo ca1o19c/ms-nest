ce1e33caee89b91045c5734aa91ea7e1c6f84dd7
Caio Cichetti <caio_cichetti@outlook.com>
2021-07-23 14:34:20 -0300

    Finished CRUD Products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { ProductService } from './product.service';
 
 @Controller('products')
 export class ProductController {
+  constructor(private productService: ProductService) {}
+
   @Get()
-  all() {
-    return { message: 'all products' };
+  async index() {
+    return this.productService.index();
+  }
+
+  @Post()
+  async store(@Body('title') title: string, @Body('image') image: string) {
+    return this.productService.store({
+      title,
+      image,
+    });
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: number) {
+    return this.productService.show(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body('title') title: string,
+    @Body('image') image: string,
+  ) {
+    return this.productService.update(id, { title, image });
+  }
+
+  @Delete(':id')
+  async destroy(@Param('id') id: number) {
+    return this.productService.destroy(id);
   }
 }
